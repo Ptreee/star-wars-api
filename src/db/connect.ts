e94fa09d1c0754a827ed53f03c6954a0eb994f48
Piotr Proszowski <60212583+Ptreee@users.noreply.github.com>
2022-05-18 00:13:38 +0200

    add envValues to connect.ts

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('starWars-API', 'postgres', '1234', {
-  host: 'localhost',
+const dbName = process.env.DB_NAME as string;
+const dbUser = process.env.DB_USER as string;
+const dbHost = process.env.DB_HOST;
+const dbPassword = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
   port: 5432,
   dialect: 'postgres',
 });
